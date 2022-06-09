b553baf76f23f404b49c4b1f76e5240b4ffb3a3d
AhmadJabrayilov <tu25tyfpp@code.edu.az>
2022-06-09 07:57:53 +0400

    featured task

---
+++ MessageApp103/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MessageApp103.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessageApp103.DAL
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+    }
+}
---
+++ MessageApp103/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MessageApp103.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string Fullname { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
